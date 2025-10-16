b118dcb4501991078b0c503d4a327e2868e97ee4
Fiona Yzabelle Gumaroy <126933200+fionayzabellegumaroy@users.noreply.github.com>
2025-10-16 16:29:24 -0400

    feature: added vite & react to project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import './generateMaze.js';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <canvas>
+
+        </canvas>
+      </div>
+      <p className="read-the-docs">
+        Escape the Maze
+      </p>
+    </>
+  )
+}
+
+export default App

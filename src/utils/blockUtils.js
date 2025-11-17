12ca75326e0ade15de354aa386587d26ba1fcd27
Fiona Yzabelle Gumaroy <126933200+fionayzabellegumaroy@users.noreply.github.com>
2025-11-16 21:49:33 -0500

    clean: cleaned up utils folder

--- src/utils/blockUtils.js
+++ src/utils/blockUtils.js
@@ -1,15 +1,15 @@
-import * as THREE from 'three';
+import { BoxGeometry, ClampToEdgeWrapping, MeshLambertMaterial, NearestFilter, SRGBColorSpace, TextureLoader } from 'three';
 import atlasUrl from '../assets/textures/textures.png';
 import { BLOCK_TYPES, ATLAS_CONFIG, BLOCK_DIMS } from '../types/blockTypes';
 
 function getTileUVs(tileX, tileY) {
-  const { tileSize, atlasWidth, atlasHeight } = ATLAS_CONFIG;
+  let { tileSize, atlasWidth, atlasHeight } = ATLAS_CONFIG;
   
   // calculate normalized UV coordinates
-  const uMin = (tileX * tileSize) / atlasWidth;
-  const uMax = ((tileX + 1) * tileSize) / atlasWidth;
-  const vMin = 1 - ((tileY + 1) * tileSize) / atlasHeight;
-  const vMax = 1 - (tileY * tileSize) / atlasHeight;
+  let uMin = (tileX * tileSize) / atlasWidth;
+  let uMax = ((tileX + 1) * tileSize) / atlasWidth;
+  let vMin = 1 - ((tileY + 1) * tileSize) / atlasHeight;
+  let vMax = 1 - (tileY * tileSize) / atlasHeight;
   
   return [
     uMin, vMax,  // bottom-left
@@ -20,18 +20,18 @@ function getTileUVs(tileX, tileY) {
 }
 
 export function createBlockGeometry(blockType) {
-  const dims = BLOCK_DIMS && BLOCK_DIMS[blockType] ? BLOCK_DIMS[blockType] : [1, 1, 1];
-  const geometry = new THREE.BoxGeometry(dims[0], dims[1], dims[2]);
+  let dims = BLOCK_DIMS && BLOCK_DIMS[blockType] ? BLOCK_DIMS[blockType] : [1, 1, 1];
+  let geometry = new BoxGeometry(dims[0], dims[1], dims[2]);
   
-  const uvs = geometry.attributes.uv.array;
-  const blockConfig = BLOCK_TYPES[blockType];
+  let uvs = geometry.attributes.uv.array;
+  let blockConfig = BLOCK_TYPES[blockType];
   
   if (!blockConfig) {
     console.warn(`Block type ${blockType} not found in BLOCK_TYPES`);
     return geometry;
   }
 
-  const faceNames = ['right', 'left', 'top', 'bottom', 'front', 'back'];
+  let faceNames = ['right', 'left', 'top', 'bottom', 'front', 'back'];
   
   geometry.clearGroups();
 
@@ -40,11 +40,11 @@ export function createBlockGeometry(blockType) {
   }
   
   for (let faceIndex = 0; faceIndex < 6; faceIndex++) {
-    const faceName = faceNames[faceIndex];
-    const [tileX, tileY] = blockConfig[faceName];
-    const faceUVs = getTileUVs(tileX, tileY);
+    let faceName = faceNames[faceIndex];
+    let [tileX, tileY] = blockConfig[faceName];
+    let faceUVs = getTileUVs(tileX, tileY);
     
-    const startIdx = faceIndex * 8;
+    let startIdx = faceIndex * 8;
     for (let i = 0; i < 8; i++) {
       uvs[startIdx + i] = faceUVs[i];
     }
@@ -54,19 +54,19 @@ export function createBlockGeometry(blockType) {
   return geometry;
 }
 
-const textureCache = new Map();
+let textureCache = new Map();
 
 function getTexture() {
   let texture = textureCache.get(atlasUrl);
   if (!texture) {
-    const loader = new THREE.TextureLoader();
+    let loader = new TextureLoader();
     texture = loader.load(atlasUrl);
     
-    texture.magFilter = THREE.NearestFilter;
-    texture.minFilter = THREE.NearestFilter;
-    texture.wrapS = THREE.ClampToEdgeWrapping;
-    texture.wrapT = THREE.ClampToEdgeWrapping;
-    texture.colorSpace = THREE.SRGBColorSpace;
+    texture.magFilter = NearestFilter;
+    texture.minFilter = NearestFilter;
+    texture.wrapS = ClampToEdgeWrapping;
+    texture.wrapT = ClampToEdgeWrapping;
+    texture.colorSpace = SRGBColorSpace;
     
     textureCache.set(atlasUrl, texture);
   }
@@ -74,26 +74,24 @@ function getTexture() {
 }
 
 export function createMaterialForType(blockType, opts = {}) {
-  const texture = getTexture();
-  const blockConfig = BLOCK_TYPES[blockType];
+  let texture = getTexture();
+  let blockConfig = BLOCK_TYPES[blockType];
   
   if (!blockConfig) {
-    return new THREE.MeshStandardMaterial({ map: texture });
+    return new MeshLambertMaterial({ map: texture });
   }
 
   // create 6 materials, one for each face
-  const materials = [];
-  const faceNames = ['right', 'left', 'top', 'bottom', 'front', 'back'];
+  let materials = [];
+  let faceNames = ['right', 'left', 'top', 'bottom', 'front', 'back'];
   
   for (let i = 0; i < 6; i++) {
-    const material = new THREE.MeshStandardMaterial({ 
+    let material = new MeshLambertMaterial({ 
       map: texture.clone(),
-      roughness: 0.8,
-      metalness: 0
     });
     
-    const [tileX, tileY] = blockConfig[faceNames[i]];
-    const { tileSize, atlasWidth, atlasHeight } = ATLAS_CONFIG;
+    let [tileX, tileY] = blockConfig[faceNames[i]];
+    let { tileSize, atlasWidth, atlasHeight } = ATLAS_CONFIG;
     
     // set texture offset and repeat for this specific tile
     material.map.repeat.set(

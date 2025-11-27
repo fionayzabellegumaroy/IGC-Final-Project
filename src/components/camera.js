6a05b27426ef3035d4a222d26142e6614709d198
fionughh <fiona.gumaroy@gmail.com>
2025-11-27 16:11:12 -0500

    refactor: changed cam settings and updated model

--- src/components/camera.js
+++ src/components/camera.js
@@ -2,14 +2,14 @@ import { PerspectiveCamera } from "three";
 import { startPosition } from "../core";
 
 export const camera = () => {
-  let camera = new PerspectiveCamera(70, 0.9, 0.1, 100);
+  let camera = new PerspectiveCamera(65, 0.9, 0.1, 100);
 
   let cellSize = 5;
 
   //this is first person POV
     camera.position.set(
       startPosition[0] * cellSize + cellSize / 2,
-      5, // Eye height
+      9, // Eye height
       startPosition[1] * cellSize + cellSize / 2
     );
 

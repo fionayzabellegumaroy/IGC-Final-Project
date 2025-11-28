4c918430cd8a34d11cb99916cd382fe0f0838f01
fionughh <fiona.gumaroy@gmail.com>
2025-11-27 23:20:08 -0500

    feat: added resizer again

--- src/systems/Resizer.js
+++ src/systems/Resizer.js
@@ -1,20 +1,20 @@
 export class Resizer {
-  // constructor(container, camera, renderer) {
-  //   this.camera = camera;
-  //   this.renderer = renderer;
-  //   this.container = container;
+  constructor(container, camera, renderer) {
+    this.camera = camera;
+    this.renderer = renderer;
+    this.container = container;
     
-  //   this.setSize();
-  //   window.addEventListener('resize', () => this.setSize());
-  // }
+    this.setSize();
+    window.addEventListener('resize', () => this.setSize());
+  }
 
-  // setSize() {
-  //   const width = this.container.clientWidth;
-  //   const height = this.container.clientHeight;
+  setSize() {
+    const width = this.container.clientWidth;
+    const height = this.container.clientHeight;
     
-  //   this.camera.aspect = width / height;
-  //   // this.camera.updateProjectionMatrix();
-  //   this.renderer.setSize(width, height);
-  //   this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-  // }
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
 }

61f16b73cb2b273163487f6a6f979ee4c6d9c106
fionughh <fiona.gumaroy@gmail.com>
2025-12-01 20:52:13 -0500

    refactor: updated some settings

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,5 +5,5 @@ export const EYE_HEIGHT = 15;
 
 export const TIME_LIMIT = 60; // seconds
 
-export const MAZE_ROWS = 4;
-export const MAZE_COLS = 4;
+export const MAZE_ROWS = 6;
+export const MAZE_COLS = 6;
